a6f51f90113fa036ff8cf75022d42199cd691c0d
saadi <102322772+saadiBrr@users.noreply.github.com>
2024-12-16 06:45:31 +0500

    Created functional grid generation

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.getElementById('grid-container');
+const generateButton = document.getElementById('generate-button');
+
+function randomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+    // Stolen code to generate a random hex
+}
+
+function createDivs(num) {
+
+    let dimension = num**2
+    let containerDim = parseInt(getComputedStyle(container).height)
+    container.innerHTML = '';
+
+    for (let i = 0; i < dimension; i++) {
+        let div = document.createElement('div');
+        div.className = 'grid';
+        div.style.backgroundColor = randomColor();
+        div.style.border = 'pink 1px dashed';
+        div.style.width = `${containerDim / num}px`;
+        div.style.height = `${containerDim / num}px`;
+        container.appendChild(div)
+    }
+}
+
+generateButton.addEventListener('click', function() {
+    createDivs(16)
+});
