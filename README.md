7adc9cfca0712b95da8027ad2c0902dbf0158647
saadi <102322772+saadiBrr@users.noreply.github.com>
2024-12-16 08:28:53 +0500

    Finish game. Add footer, CSS, JS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-etch-a-sketch
+
+This is a simple implementation of the classic Etch A Sketch game. The game is played on a dynamically generated grid, where the user uses the cursor to draw. This is a project made for the [The Odin Project](https://theodinproject.com).
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.getElementById('grid-container');
 const generateButton = document.getElementById('generate-button');
 const resetButton = document.getElementById('reset-button');
+let gridSize = 0;
 
 function randomColor() {
     return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
@@ -9,25 +10,42 @@ function randomColor() {
 
 function createDivs(num) {
 
-    let dimension = num**2
+    let dimension = num ** 2
     let containerDim = parseInt(getComputedStyle(container).height)
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
         let div = document.createElement('div');
         div.className = 'grid';
-        div.style.backgroundColor = randomColor();
-        div.style.border = 'pink 1px dashed';
+        // div.style.backgroundColor = randomColor();
+        // div.style.border = 'yellow 1px solid';
         div.style.width = `${containerDim / num}px`;
         div.style.height = `${containerDim / num}px`;
         container.appendChild(div)
     }
 }
 
-generateButton.addEventListener('click', function() {
-    createDivs(16)
+function draw(event) {
+    let target = event.target;
+
+    if (target.className === 'grid') {
+        target.classList.add('active')
+    }
+}
+
+generateButton.addEventListener('click', function () {
+    gridSize = prompt('Enter grid size');
+
+    gridSize <= 0 ? gridSize = 16 : gridSize
+    gridSize < 2 ? gridSize = 2 : gridSize;
+    gridSize > 100 ? gridSize = 100 : gridSize;
+
+    createDivs(gridSize);
 });
 
-resetButton.addEventListener('click', function() {
+resetButton.addEventListener('click', function () {
     container.innerHTML = '';
+    createDivs(gridSize);
 })
+
+container.addEventListener('mouseover', draw)
